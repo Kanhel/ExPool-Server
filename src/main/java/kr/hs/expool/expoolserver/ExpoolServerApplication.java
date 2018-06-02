48ba5f512d6da21b7c9f0ece14728c146948afc3
MinwookJo <alsdnr0537@naver.com>
2018-06-02 20:39:51 +0900

    Init

---
+++ src/main/java/kr/hs/expool/expoolserver/ExpoolServerApplication.java
@@ -0,0 +1,12 @@
+package kr.hs.expool.expoolserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpoolServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpoolServerApplication.class, args);
+	}
+}
---
+++ src/test/java/kr/hs/expool/expoolserver/ExpoolServerApplicationTests.java
@@ -0,0 +1,16 @@
+package kr.hs.expool.expoolserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExpoolServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
